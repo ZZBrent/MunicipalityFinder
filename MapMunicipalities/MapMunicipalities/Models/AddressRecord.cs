62480eae8264f3e7aae394f584228a82ae9a19ed
Brent Keath <brentkeath@gmail.com>
2025-01-28 09:11:20 -0500

    Initial application build
    The initial application which will grab all of the municipalities for a list of addresses using the U.S. census data.

---
+++ MapMunicipalities/MapMunicipalities/Models/AddressRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapMunicipalities.Models
+{
+    public class AddressRecord
+    {
+        public string Address { get; set; }
+    }
+
+}
